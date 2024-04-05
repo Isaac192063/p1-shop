4a84f8c3219f32c56442df68e82ac59a698b3fdd
CarlosMolina07 <camolinar@ufpso.edu.co>
2024-04-04 21:05:10 -0500

    inicializacion del proyecto

--- src/main/java/com/primer_parcial/shop/ShopApplication.java
+++ src/main/java/com/primer_parcial/shop/ShopApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApplication {
-    public String nameubhuh;
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
