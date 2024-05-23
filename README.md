8f71318c0bb1fa683561c1e5940b64beb01187ba
isaac192063 <ichinchillac@ufpso.edu.co>
2024-05-23 17:46:48 -0500

    actualizacion de response de articulo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Tienda Virtual
+Tienda Virtual - segundo parcial
 
 Isaac Chinchilla 192063
 Carlos molina 191926
--- src/main/java/com/primer_parcial/shop/controller/CategoryController.java
+++ src/main/java/com/primer_parcial/shop/controller/CategoryController.java
@@ -1,7 +1,9 @@
 package com.primer_parcial.shop.controller;
 
+import com.primer_parcial.shop.model.Article;
 import com.primer_parcial.shop.model.Category;
 import com.primer_parcial.shop.model.dto.Request;
+import com.primer_parcial.shop.model.dto.Response;
 import com.primer_parcial.shop.service.category.CategoryServiceImp;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
@@ -10,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -20,9 +23,13 @@ public class CategoryController {
     private CategoryServiceImp categoryService;
 
     @PostMapping()
-    public ResponseEntity<Category> createCategory(@RequestBody @Valid Request<Category> categoryRequest){
+    public ResponseEntity<Response<Category>> createCategory(@RequestBody @Valid Request<Category> categoryRequest){
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(categoryService.createCategory(categoryRequest.getRequestMessage().getData()));
+                .body(Response.<Category>builder()
+                                .date(LocalDateTime.now())
+                                .message(categoryService.createCategory(categoryRequest.getRequestMessage().getData()))
+                                .statusCode(HttpStatus.CREATED.value())
+                                .build());
     }
 
     @GetMapping()
@@ -31,14 +38,26 @@ public class CategoryController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> updateCategory(
+    public ResponseEntity<Response<Category>> updateCategory(
             @RequestBody @Valid Request<Category> categoryRequest, @PathVariable @Positive Long id){
-        return ResponseEntity.ok().body(categoryService.updateCategory(id, categoryRequest.getRequestMessage().getData()));
+        return ResponseEntity.ok().body(
+                Response.<Category>builder()
+                        .date(LocalDateTime.now())
+                        .message(categoryService.updateCategory(id, categoryRequest.getRequestMessage().getData()))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+                );
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Category> getCategoryById(@PathVariable @Positive Long  id){
-        return ResponseEntity.ok().body(categoryService.getCategoryById(id));
+    public ResponseEntity<Response<Category>> getCategoryById(@PathVariable @Positive Long  id){
+        return ResponseEntity.ok().body(
+                Response.<Category>builder()
+                        .date(LocalDateTime.now())
+                        .message(categoryService.getCategoryById(id))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+                );
     }
 
 
